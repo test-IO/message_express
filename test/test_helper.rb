c1c245d35c31a05253312a9daac6e5d6724e8125
Simon Lacroix <simon@miaou.be>
2019-05-10 15:23:27 +0200

    Setup rubocop (#2)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "message_express"
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+require 'message_express'
 
-require "minitest/autorun"
+require 'minitest/autorun'
