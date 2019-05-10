90e276c126a8d019b9652d3be435219174a87b69
Simon Lacroix <simon@miaou.be>
2019-05-10 14:50:49 +0200

    Remove failing test

--- test/message_express_test.rb
+++ test/message_express_test.rb
@@ -4,8 +4,4 @@ class MessageExpressTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::MessageExpress::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
