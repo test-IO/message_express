1866b39f440dc9ec199ce20062f8af89995f099d
Inderpal Singh <singhinther@gmail.com>
2020-02-26 11:17:55 +0100

    option to add sidekiq queue

--- lib/message_express/async_publisher.rb
+++ lib/message_express/async_publisher.rb
@@ -4,6 +4,7 @@ module MessageExpress
   class AsyncPublisher
     include MessageExpress::Publisher
     include Sidekiq::Worker
+    sidekiq_options queue: ENV['MESSAGE_EXPRESS_SIDEKIQ_QUEUE'] || 'default'
 
     def perform(message_name, payload)
       publish(message_name, payload, validate: false)
